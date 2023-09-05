f8a20ec5b833584b6d7753a1b22747c2f352e6c1
madortill <mador.till@gmail.com>
2023-09-05 18:31:05 +0300

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
