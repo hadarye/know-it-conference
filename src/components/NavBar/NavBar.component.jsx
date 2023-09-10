311bfc1f51ac9b9d816ef653b8e826ff3ba9cc6c
madortill <mador.till@gmail.com>
2023-09-10 16:59:09 +0300

    url try 2

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -46,7 +46,7 @@ const NavBar = (props) => {
           <span></span>
         </div>
         {/* <img src={menu} className='menu-icon pointer' onClick={() => props.handleMenu()}/> */}
-        <img src={LOGO} onClick={() => navigate("/home")} className='know-it-logo pointer'/>
+        <img src={LOGO} onClick={() => navigate("knowit-2023/home")} className='know-it-logo pointer'/>
       </div>
       <Outlet></Outlet>
 
