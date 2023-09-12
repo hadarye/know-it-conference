dc81f398125085daac9bc7e22eaa14c8bdfd63bc
madortill <mador.till@gmail.com>
2023-09-12 20:29:57 +0300

    toast

--- src/components/ParticipantCard/ParticipantCard.component.jsx
+++ src/components/ParticipantCard/ParticipantCard.component.jsx
@@ -8,7 +8,7 @@ const ParticipantCard = (props) => {
     return (
         <div className='participant-card-container' onClick={(e) => e.target.classList.contains("not-press") ? null : props.closeReadMore()}>
             <div className='participant-card not-press'>
-                <div style={{ backgroundImage: `url(${close})` }} className='card-cose-btn'></div>
+                <div style={{ backgroundImage: `url(${close})` }} className='card-cose-btn pointer'></div>
                 <img src={props.infoObj.img} className='main-img participant-card-img not-press'></img>
                 <h1 className='participant-card-topic not-press'>{props.infoObj.topic}</h1>
                 <h3 className='participant-card-name not-press'>{props.infoObj.name}</h3>
