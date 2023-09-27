4db1eb814f764ffeecd2c953e31795583eae76aa
madortill <mador.till@gmail.com>
2023-09-27 16:54:30 +0300

    confirmation

--- src/components/Confirmation/Confirmation.component.jsx
+++ src/components/Confirmation/Confirmation.component.jsx
@@ -6,7 +6,8 @@ const Confirmation = () => {
     return(
         <div className='confirmation-container'>
             <div className='confirmation-card'>
-                <h1 className='confirmation-title'>הרשמתך נקלטה במערכת!</h1>
+                <h1 className='confirmation-title'>אנא בדקו שקיבלתם מייל אישור.</h1>
+                <h2 className='confirmation-text'>לשאלות: <a className='mail' href="mailto:know.it.till@gmail.com">know.it.till@gmail.com</a></h2>
                 <button className='sign-btn confirmation-btn pointer' onClick={() => {navigate("/home"); window.scrollTo({top: 0, left: 0, behavior: 'smooth'});}}>חזרה לדף הראשי</button>
             </div>
         </div>
